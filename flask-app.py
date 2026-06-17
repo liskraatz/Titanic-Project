d0521fb911783e5642135b4a3b7c006d67c45bc4
Lisa Kraatz <liskraatz@gmail.com>
2026-06-17 14:40:56 +0200

    Finished flask app for now - continuing with HTML and JS

--- flask-app.py
+++ flask-app.py
@@ -8,6 +8,25 @@ titanicModel = file
 # Creating application object (server)
 app = Flask(__name__) # tells flask to search in current script
 
+# Catching JSON data sent by JS
+@app.route('/predict', methods=['POST']) # 
+def predict_survival():
+  data = request.get_json()
+  name = data.get('name')
+  sex = data.get('sex')
+  age = data.get('age')
+  print(age)
+  title = data.get('title')
+  travelComfort = data.get('travel-comfort')
+  siblings = data.get('sib')
+  parents = data.get('pa')
+  spouses = data.get('sp')
+  children = data.get('ch')
+  jsonify({'Status':'Received'}) # Web Browser needs valid return statement - without it function would've returned 'None'
+  
+
+
+
 
 
 
